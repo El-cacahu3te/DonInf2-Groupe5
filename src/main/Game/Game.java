09e8993882e3f787cfe6a09bb7f124454ba15880
elise2025ctrl <elise.kovacs@heig-vd.ch>
2025-06-04 11:11:07 +0200

    ajout Zones
    ajout de 3 zones

--- src/main/Game/Game.java
+++ src/main/Game/Game.java
@@ -6,7 +6,7 @@ public class Game {
     CommandRegistry command;
 
         public void init() {
-            this.map = new WorldMap(3,3);
+            this.map = new WorldMap(3,4);
             Zone cuisine = new Zone("La cuisine", "Ici c'est la cuisine, on y fait de bon petits plats", true);
             Zone chambre = new Zone("La chambre", "Un endroit cosy et tranquille, parfait pour se reposer après une longue journée d'aventure.", false);
             Zone poulailler = new Zone("Le poulailler", "Les poules caquettent joyeusement ici. On y trouve des œufs frais chaque matin.", true);
@@ -16,6 +16,9 @@ public class Game {
             Zone riviere = new Zone("La rivière", "L'eau claire serpente doucement, apportant fraîcheur et sérénité. On y entend le doux murmure du courant.", true);
             Zone leMassif = new Zone("Le massif de fleurs", "Un bouquet vivant de couleurs éclatantes et de parfums enivrants. Papillons et abeilles y dansent.", true);
             Zone jardin = new Zone("Le jardin", "Le jardin est un joli endroit, plein d'herbes et de plantes sauvages, fait pour que les petits et les grands puissent passer du bon temps.", true);
+            Zone petanque = new Zone("La petanque", "Un petit espace graviollonné parfait pour partager un pastis en jouant une partie de boules.", true);
+            Zone piscine = new Zone ("La piscine", "Un plongeon ? Faire bronzette au bord de la piscine ? Cette piscine aux eaux turquoise n'as synonyme qu'un seul mot : détente.", true);
+            Zone trouNoir = new Zone ("Le trou noir", "La légende dit que personne n'est jamais resortie de cette zone. A vos risques et perils.", true);
             map.addZone(chambre, 0, 0);
             map.addZone(leMassif, 1, 0);
             map.addZone(riviere, 2, 0);
@@ -25,6 +28,9 @@ public class Game {
             map.addZone(poulailler, 0, 2);
             map.addZone(verger, 1, 2);
             map.addZone(foret, 2, 2);
+            map.addZone(petanque,0,3);
+            map.addZone(piscine,1,3);
+            map.addZone(trouNoir,2,3);
         }
 
         public void run(){
